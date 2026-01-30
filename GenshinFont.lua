2856593d94fc289165d835e3207163de7c4c84f9
jake-t-dev <krysis15@outlook.com>
2026-01-30 21:24:16 +0000

    init

---
+++ GenshinFont.lua
@@ -0,0 +1,20 @@
+
+GenshinFont = CreateFrame("Frame", "GenshinFont");
+
+local damagefont_FONT_NUMBER = "Interface\\AddOns\\GenshinFont\\font.ttf";
+
+function GenshinFont:ApplySystemFonts()
+
+DAMAGE_TEXT_FONT = damagefont_FONT_NUMBER;
+
+end
+
+GenshinFont:SetScript("OnEvent",
+		    function() 
+		       if (event == "ADDON_LOADED") then
+			  GenshinFont:ApplySystemFonts()
+		       end
+		    end);
+GenshinFont:RegisterEvent("ADDON_LOADED");
+
+GenshinFont:ApplySystemFonts()
