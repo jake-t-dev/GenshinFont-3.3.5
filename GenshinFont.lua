cb87ec36b3e007bce1d2b976501d6026e942a3e7
jake-t-dev <krysis15@outlook.com>
2026-01-30 21:48:45 +0000

    updates

--- GenshinFont.lua
+++ GenshinFont.lua
@@ -1,20 +1,51 @@
-
 GenshinFont = CreateFrame("Frame", "GenshinFont");
 
-local damagefont_FONT_NUMBER = "Interface\\AddOns\\GenshinFont-3.3.5\\font.ttf";
+local fontPath = "Interface\\AddOns\\GenshinFont-3.3.5\\font.ttf";
 
 function GenshinFont:ApplySystemFonts()
+    -- Damage Numbers
+    DAMAGE_TEXT_FONT = fontPath;
 
-DAMAGE_TEXT_FONT = damagefont_FONT_NUMBER;
-
+    -- Main UI Fonts
+    UIDP_Font = fontPath; 
+    SystemFont_Tiny = fontPath;
+    SystemFont_Small = fontPath;
+    SystemFont_Outline_Small = fontPath;
+    SystemFont_Shadow_Small = fontPath;
+    SystemFont_Inverse_Small = fontPath;
+    SystemFont_Med1 = fontPath;
+    SystemFont_Med2 = fontPath;
+    SystemFont_Med3 = fontPath;
+    SystemFont_Shadow_Med1 = fontPath;
+    SystemFont_Shadow_Med2 = fontPath;
+    SystemFont_Shadow_Med3 = fontPath;
+    SystemFont_Large = fontPath;
+    SystemFont_Shadow_Large = fontPath;
+    SystemFont_Huge1 = fontPath;
+    SystemFont_Huge2 = fontPath;
+    SystemFont_Shadow_Huge1 = fontPath;
+    SystemFont_Shadow_Huge2 = fontPath;
+    SystemFont_Shadow_Huge3 = fontPath;
+    
+    -- Chat and Input
+    ChatFontNormal = fontPath;
+    
+    -- Headers
+    QuestFont = fontPath;
+    QuestFont_Large = fontPath;
+    QuestFont_Huge = fontPath;
+    QuestFont_Shadow_Huge = fontPath;
+    QuestFont_Super_Huge = fontPath;
+    QuestFont_Shadow_Small = fontPath;
 end
 
-GenshinFont:SetScript("OnEvent",
-		    function() 
-		       if (event == "ADDON_LOADED") then
-			  GenshinFont:ApplySystemFonts()
-		       end
-		    end);
+GenshinFont:SetScript("OnEvent", function() 
+    if (event == "ADDON_LOADED") then
+        GenshinFont:ApplySystemFonts()
+    end
+end);
+
 GenshinFont:RegisterEvent("ADDON_LOADED");
 
+-- Apply
 GenshinFont:ApplySystemFonts()
